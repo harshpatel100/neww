42f1c2c06d1ef332fec92c3029b3b2572f3ece83
harshmpatel1 <patelharsh7458@gmail.com>
2021-10-03 13:13:23 +0530

    added

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const sp = path.join(__dirname,'/public')
+const port = process.env.PORT
+app.use(express.static(sp))
+app.get('',(req,res)=>{
+  res.render('index')
+})
+app.listen(3000 || port,()=>{
+  console.log("ok server is running..")
+})
